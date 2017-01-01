38bef7ce42a584cebf6512a3b5a92144b88b2907
hensoul <hensoul.huang@hotmail.com>
2017-01-01 23:06:54 +0800

    Signed-off-by: hensoul <hensoul.huang@hotmail.com>

--- src/main/java/com/lcpa/lclove/web/controller/LoginControl.java
+++ src/main/java/com/lcpa/lclove/web/controller/LoginControl.java
@@ -2,6 +2,9 @@ package com.lcpa.lclove.web.controller;
 
 import javax.servlet.http.HttpSession;
 
+import com.lcpa.lclove.model.User;
+import com.lcpa.lclove.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +13,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by shao on 2016/12/25.
  */
 @Controller
-public class LoginControl {
+public class LoginControl extends AnnotationController{
 
-//    @RequestMapping(value="/admin/login.action")
-//    public String login(HttpSession session, ModelMap model, String username,String password) throws Exception{
-//        //TODO
-//        //在Session里保存信息
-//        session.setAttribute("username", username);
-//        //重定向
-//        String loginSuccessRedirect = session.getAttribute("loginSuccessRedirect").toString();
-//        return "redirect:"+loginSuccessRedirect;
-//    }
-//
+    @Autowired
+    public UserService userService;
+
+    @RequestMapping(value="/admin/login.action")
+    public String login(HttpSession session, ModelMap model, String username,String password) throws Exception{
+        //TODO
+        //在Session里保存信息
+        User user = userService.getUserByName(username);
+        if (user.getPassword().equals(password)){
+            session.setAttribute("username", username);
+            //重定向
+            String loginSuccessRedirect = session.getAttribute("loginSuccessRedirect").toString();
+            return "redirect:"+loginSuccessRedirect;
+        }else{
+            return showJsonError(model, "账号密码错误！");
+        }
+    }
+
 //    @RequestMapping(value="/admin/changeUser")
 //    public String changeUser(HttpSession session, ModelMap model, String username,String password) throws Exception{
 //        //TODO
@@ -31,12 +42,12 @@ public class LoginControl {
 //        String loginSuccessRedirect = session.getAttribute("loginSuccessRedirect").toString();
 //        return "redirect:"+loginSuccessRedirect;
 //    }
-//
-//    @RequestMapping(value="/logout")
-//    public String logout(HttpSession session) throws Exception{
-//        //清除Session
-//        session.invalidate();
-//
-//        return "redirect:hello.action";
-//    }
+
+    @RequestMapping(value="/logout")
+    public String logout(HttpSession session) throws Exception{
+        //清除Session
+        session.invalidate();
+
+        return "redirect:hello.action";
+    }
 }
