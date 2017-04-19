bb00ab7f6145bea6d6201f40551d85aae45a521a
hensoul <hensoul.huang@hotmail.com>
2017-04-19 17:16:31 +0800

    Signed-off-by: hensoul <hensoul.huang@hotmail.com>

--- src/main/webapp/js/lclove-util.js
+++ src/main/webapp/js/lclove-util.js
@@ -1,3 +1,21 @@
+var system = {
+    win: false,
+    mac: false,
+    xll: false,
+    ipad:false
+};
+//检测平台
+var p = navigator.platform;
+system.win = p.indexOf("Win") == 0;
+system.mac = p.indexOf("Mac") == 0;
+system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+
+if (system.win || system.mac || system.xll||system.ipad) {
+} else {
+    window.location.href="http://www.lclianmao.com/mobile/index.asp";
+}
+
 var lclove = {};
 lclove.util = {};
 lclove.util.basePath = '/';
